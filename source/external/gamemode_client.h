4feaaca19cfb1a653a9302843d942ad78d49099e
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-08-27 12:50:58 -0300

    Refactor gamemode_client.h for improved structure

--- source/external/gamemode_client.h
+++ source/external/gamemode_client.h
@@ -30,49 +30,6 @@ POSSIBILITY OF SUCH DAMAGE.
  */
 #ifndef CLIENT_GAMEMODE_H
 #define CLIENT_GAMEMODE_H
-/*
- * GameMode supports the following client functions
- * Requests are refcounted in the daemon
- *
- * int gamemode_request_start() - Request gamemode starts
- *   0 if the request was sent successfully
- *   -1 if the request failed
- *
- * int gamemode_request_end() - Request gamemode ends
- *   0 if the request was sent successfully
- *   -1 if the request failed
- *
- * GAMEMODE_AUTO can be defined to make the above two functions apply during static init and
- * destruction, as appropriate. In this configuration, errors will be printed to stderr
- *
- * int gamemode_query_status() - Query the current status of gamemode
- *   0 if gamemode is inactive
- *   1 if gamemode is active
- *   2 if gamemode is active and this client is registered
- *   -1 if the query failed
- *
- * int gamemode_request_start_for(pid_t pid) - Request gamemode starts for another process
- *   0 if the request was sent successfully
- *   -1 if the request failed
- *   -2 if the request was rejected
- *
- * int gamemode_request_end_for(pid_t pid) - Request gamemode ends for another process
- *   0 if the request was sent successfully
- *   -1 if the request failed
- *   -2 if the request was rejected
- *
- * int gamemode_query_status_for(pid_t pid) - Query status of gamemode for another process
- *   0 if gamemode is inactive
- *   1 if gamemode is active
- *   2 if gamemode is active and this client is registered
- *   -1 if the query failed
- *
- * const char* gamemode_error_string() - Get an error string
- *   returns a string describing any of the above errors
- *
- * Note: All the above requests can be blocking - dbus requests can and will block while the daemon
- * handles the request. It is not recommended to make these calls in performance critical code
- */
 
 #include <stdbool.h>
 #include <stdio.h>
@@ -81,25 +38,32 @@ POSSIBILITY OF SUCH DAMAGE.
 #include <string.h>
 
 #include <assert.h>
+#include <errno.h>
+#include <time.h>
 
 #include <sys/types.h>
+#include <unistd.h>
+
+#ifdef __cplusplus
+#include <atomic>
+#include <mutex>
+#endif
 
 static char internal_gamemode_client_error_string[512] = { 0 };
 
-/**
- * Load libgamemode dynamically to dislodge us from most dependencies.
- * This allows clients to link and/or use this regardless of runtime.
- * See SDL2 for an example of the reasoning behind this in terms of
- * dynamic versioning as well.
- */
 static volatile int internal_libgamemode_loaded = 1;
 
-/* Typedefs for the functions to load */
+#ifdef __cplusplus
+static std::mutex internal_gamemode_load_mutex;
+#else
+#include <pthread.h>
+static pthread_mutex_t internal_gamemode_load_mutex = PTHREAD_MUTEX_INITIALIZER;
+#endif
+
 typedef int (*api_call_return_int)(void);
 typedef const char *(*api_call_return_cstring)(void);
 typedef int (*api_call_pid_return_int)(pid_t);
 
-/* Storage for functors */
 static api_call_return_int REAL_internal_gamemode_request_start = NULL;
 static api_call_return_int REAL_internal_gamemode_request_end = NULL;
 static api_call_return_int REAL_internal_gamemode_query_status = NULL;
@@ -108,18 +72,12 @@ static api_call_pid_return_int REAL_internal_gamemode_request_start_for = NULL;
 static api_call_pid_return_int REAL_internal_gamemode_request_end_for = NULL;
 static api_call_pid_return_int REAL_internal_gamemode_query_status_for = NULL;
 
-/**
- * Internal helper to perform the symbol binding safely.
- *
- * Returns 0 on success and -1 on failure
- */
 __attribute__((always_inline)) static inline int internal_bind_libgamemode_symbol(
     void *handle, const char *name, void **out_func, size_t func_size, bool required)
 {
 	void *symbol_lookup = NULL;
 	char *dl_error = NULL;
 
-	/* Safely look up the symbol */
 	symbol_lookup = dlsym(handle, name);
 	dl_error = dlerror();
 	if (required && (dl_error || !symbol_lookup)) {
@@ -130,24 +88,16 @@ __attribute__((always_inline)) static inline int internal_bind_libgamemode_symbo
 		return -1;
 	}
 
-	/* Have the symbol correctly, copy it to make it usable */
 	memcpy(out_func, &symbol_lookup, func_size);
 	return 0;
 }
 
-/**
- * Loads libgamemode and needed functions
- *
- * Returns 0 on success and -1 on failure
- */
-__attribute__((always_inline)) static inline int internal_load_libgamemode(void)
+__attribute__((always_inline)) static inline int internal_load_libgamemode_unlocked(void)
 {
-	/* We start at 1, 0 is a success and -1 is a fail */
 	if (internal_libgamemode_loaded != 1) {
 		return internal_libgamemode_loaded;
 	}
 
-	/* Anonymous struct type to define our bindings */
 	struct binding {
 		const char *name;
 		void **functor;
@@ -186,12 +136,8 @@ __attribute__((always_inline)) static inline int internal_load_libgamemode(void)
 
 	void *libgamemode = NULL;
 
-	/* Try and load libgamemode */
 	libgamemode = dlopen("libgamemode.so.0", RTLD_NOW);
 	if (!libgamemode) {
-		/* Attempt to load unversioned library for compatibility with older
-		 * versions (as of writing, there are no ABI changes between the two -
-		 * this may need to change if ever ABI-breaking changes are made) */
 		libgamemode = dlopen("libgamemode.so", RTLD_NOW);
 		if (!libgamemode) {
 			snprintf(internal_gamemode_client_error_string,
@@ -203,7 +149,6 @@ __attribute__((always_inline)) static inline int internal_load_libgamemode(void)
 		}
 	}
 
-	/* Attempt to bind all symbols */
 	for (size_t i = 0; i < sizeof(bindings) / sizeof(bindings[0]); i++) {
 		struct binding *binder = &bindings[i];
 
@@ -217,33 +162,37 @@ __attribute__((always_inline)) static inline int internal_load_libgamemode(void)
 		};
 	}
 
-	/* Success */
 	internal_libgamemode_loaded = 0;
 	return 0;
 }
 
-/**
- * Redirect to the real libgamemode
- */
+__attribute__((always_inline)) static inline int internal_load_libgamemode(void)
+{
+	int result;
+
+#ifdef __cplusplus
+	std::lock_guard<std::mutex> lock(internal_gamemode_load_mutex);
+	result = internal_load_libgamemode_unlocked();
+#else
+	pthread_mutex_lock(&internal_gamemode_load_mutex);
+	result = internal_load_libgamemode_unlocked();
+	pthread_mutex_unlock(&internal_gamemode_load_mutex);
+#endif
+
+	return result;
+}
+
 __attribute__((always_inline)) static inline const char *gamemode_error_string(void)
 {
-	/* If we fail to load the system gamemode, or we have an error string already, return our error
-	 * string instead of diverting to the system version */
 	if (internal_load_libgamemode() < 0 || internal_gamemode_client_error_string[0] != '\0') {
 		return internal_gamemode_client_error_string;
 	}
 
-	/* Assert for static analyser that the function is not NULL */
 	assert(REAL_internal_gamemode_error_string != NULL);
 
 	return REAL_internal_gamemode_error_string();
 }
 
-/**
- * Redirect to the real libgamemode
- * Allow automatically requesting game mode
- * Also prints errors as they happen.
- */
 #ifdef GAMEMODE_AUTO
 __attribute__((constructor))
 #else
@@ -251,7 +200,6 @@ __attribute__((always_inline)) static inline
 #endif
 int gamemode_request_start(void)
 {
-	/* Need to load gamemode */
 	if (internal_load_libgamemode() < 0) {
 #ifdef GAMEMODE_AUTO
 		fprintf(stderr, "gamemodeauto: %s\n", gamemode_error_string());
@@ -259,7 +207,6 @@ int gamemode_request_start(void)
 		return -1;
 	}
 
-	/* Assert for static analyser that the function is not NULL */
 	assert(REAL_internal_gamemode_request_start != NULL);
 
 	if (REAL_internal_gamemode_request_start() < 0) {
@@ -272,7 +219,6 @@ int gamemode_request_start(void)
 	return 0;
 }
 
-/* Redirect to the real libgamemode */
 #ifdef GAMEMODE_AUTO
 __attribute__((destructor))
 #else
@@ -280,7 +226,6 @@ __attribute__((always_inline)) static inline
 #endif
 int gamemode_request_end(void)
 {
-	/* Need to load gamemode */
 	if (internal_load_libgamemode() < 0) {
 #ifdef GAMEMODE_AUTO
 		fprintf(stderr, "gamemodeauto: %s\n", gamemode_error_string());
@@ -288,7 +233,6 @@ int gamemode_request_end(void)
 		return -1;
 	}
 
-	/* Assert for static analyser that the function is not NULL */
 	assert(REAL_internal_gamemode_request_end != NULL);
 
 	if (REAL_internal_gamemode_request_end() < 0) {
@@ -301,10 +245,8 @@ int gamemode_request_end(void)
 	return 0;
 }
 
-/* Redirect to the real libgamemode */
 __attribute__((always_inline)) static inline int gamemode_query_status(void)
 {
-	/* Need to load gamemode */
 	if (internal_load_libgamemode() < 0) {
 		return -1;
 	}
@@ -319,10 +261,8 @@ __attribute__((always_inline)) static inline int gamemode_query_status(void)
 	return REAL_internal_gamemode_query_status();
 }
 
-/* Redirect to the real libgamemode */
 __attribute__((always_inline)) static inline int gamemode_request_start_for(pid_t pid)
 {
-	/* Need to load gamemode */
 	if (internal_load_libgamemode() < 0) {
 		return -1;
 	}
@@ -337,10 +277,8 @@ __attribute__((always_inline)) static inline int gamemode_request_start_for(pid_
 	return REAL_internal_gamemode_request_start_for(pid);
 }
 
-/* Redirect to the real libgamemode */
 __attribute__((always_inline)) static inline int gamemode_request_end_for(pid_t pid)
 {
-	/* Need to load gamemode */
 	if (internal_load_libgamemode() < 0) {
 		return -1;
 	}
@@ -355,10 +293,8 @@ __attribute__((always_inline)) static inline int gamemode_request_end_for(pid_t 
 	return REAL_internal_gamemode_request_end_for(pid);
 }
 
-/* Redirect to the real libgamemode */
 __attribute__((always_inline)) static inline int gamemode_query_status_for(pid_t pid)
 {
-	/* Need to load gamemode */
 	if (internal_load_libgamemode() < 0) {
 		return -1;
 	}
@@ -372,5 +308,146 @@ __attribute__((always_inline)) static inline int gamemode_query_status_for(pid_t
 
 	return REAL_internal_gamemode_query_status_for(pid);
 }
+
+__attribute__((always_inline)) static inline bool gamemode_is_supported(void)
+{
+	return internal_load_libgamemode() == 0;
+}
+
+__attribute__((always_inline)) static inline int gamemode_request_start_retry(int attempts,
+                                                                                int delay_ms)
+{
+	int result = -1;
+
+	for (int i = 0; i < attempts; i++) {
+		result = gamemode_request_start();
+		if (result == 0) {
+			return 0;
+		}
+
+		struct timespec ts;
+		ts.tv_sec = delay_ms / 1000;
+		ts.tv_nsec = (delay_ms % 1000) * 1000000L;
+		nanosleep(&ts, NULL);
+	}
+
+	return result;
+}
+
+__attribute__((always_inline)) static inline bool gamemode_is_active_for_self(void)
+{
+	return gamemode_query_status() >= 1;
+}
+
+__attribute__((always_inline)) static inline bool gamemode_is_registered_for_self(void)
+{
+	return gamemode_query_status() == 2;
+}
+
+#ifdef __cplusplus
+
+class GameModeScope
+{
+      public:
+	explicit GameModeScope(bool autoStart = true) : m_active(false)
+	{
+		if (autoStart) {
+			start();
+		}
+	}
+
+	~GameModeScope()
+	{
+		end();
+	}
+
+	GameModeScope(const GameModeScope &) = delete;
+	GameModeScope &operator=(const GameModeScope &) = delete;
+
+	bool start()
+	{
+		if (m_active) {
+			return true;
+		}
+
+		if (gamemode_request_start() == 0) {
+			m_active = true;
+		}
+
+		return m_active;
+	}
+
+	void end()
+	{
+		if (m_active) {
+			gamemode_request_end();
+			m_active = false;
+		}
+	}
+
+	bool isActive() const
+	{
+		return m_active;
+	}
+
+	static const char *lastError()
+	{
+		return gamemode_error_string();
+	}
+
+      private:
+	bool m_active;
+};
+
+class GameModeScopeFor
+{
+      public:
+	explicit GameModeScopeFor(pid_t pid, bool autoStart = true) : m_pid(pid), m_active(false)
+	{
+		if (autoStart) {
+			start();
+		}
+	}
+
+	~GameModeScopeFor()
+	{
+		end();
+	}
+
+	GameModeScopeFor(const GameModeScopeFor &) = delete;
+	GameModeScopeFor &operator=(const GameModeScopeFor &) = delete;
+
+	bool start()
+	{
+		if (m_active) {
+			return true;
+		}
+
+		if (gamemode_request_start_for(m_pid) == 0) {
+			m_active = true;
+		}
+
+		return m_active;
+	}
+
+	void end()
+	{
+		if (m_active) {
+			gamemode_request_end_for(m_pid);
+			m_active = false;
+		}
+	}
+
+	bool isActive() const
+	{
+		return m_active;
+	}
+
+      private:
+	pid_t m_pid;
+	bool m_active;
+};
+
+#endif // __cplusplus
 
 #endif // CLIENT_GAMEMODE_H
