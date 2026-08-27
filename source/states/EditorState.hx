111f8864953ccc0907ff5e125fd486807c18f34d
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-08-27 20:50:46 -0300

    Refactor EditorState to manage editor options

--- source/states/EditorState.hx
+++ source/states/EditorState.hx
@@ -3,69 +3,128 @@ package states;
 import flixel.FlxG;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
+import flixel.group.FlxGroup.FlxTypedGroup;
+
+import states.editors.LevelEditorState;
+
+typedef EditorOption =
+{
+	label:String,
+	action:Void->Void
+}
 
 class EditorState extends GameState
 {
-    private var info:FlxText;
+	private var title:FlxText;
+	private var info:FlxText;
+	private var optionTexts:FlxTypedGroup<FlxText>;
+	private var options:Array<EditorOption>;
+	private var selectedIndex:Int = 0;
 
-    #if mobile
-    public var manager:MobileInputManager;
-    #end
+	#if mobile
+	public var manager:MobileInputManager;
+	#end
 
-    public function new():Void
-    {
-        super();
-        #if mobile
-        manager = new MobileInputManager();
-        #end
-    }
+	public function new():Void
+	{
+		super();
+		#if mobile
+		manager = new MobileInputManager();
+		#end
+	}
 
-    override public function create():Void
-    {
-        super.create();
+	override public function create():Void
+	{
+		super.create();
 
-        FlxG.camera.bgColor = FlxColor.BLACK;
+		FlxG.camera.bgColor = FlxColor.BLACK;
 
-        var title:FlxText = new FlxText(
-            0,
-            80,
-            FlxG.width,
-            'ALI A GAME EDITOR',
-            32
-        );
-        title.setFormat(null, 32, FlxColor.WHITE, CENTER);
-        add(title);
+		buildOptions();
 
-        info = new FlxText(
-            80,
-            180,
-            FlxG.width - 160,
-            '',
-            20
-        );
+		title = new FlxText(0, 80, FlxG.width, 'ALI A GAME EDITOR', 32);
+		title.setFormat(null, 32, FlxColor.WHITE, CENTER);
+		add(title);
 
-        info.text =
-            'EDITOR\n\n' +
-            '- Level Editor\n' +
-            '- Character Editor\n' +
-            '- Animation Editor\n' +
-            '- Script Editor\n\n' +
-            'ESC - Return';
+		optionTexts = new FlxTypedGroup<FlxText>();
+		add(optionTexts);
 
-        info.setFormat(null, 20, FlxColor.WHITE, CENTER);
-        add(info);
+		for (i in 0...options.length)
+		{
+			var entry = new FlxText(0, 180 + (i * 40), FlxG.width, options[i].label, 24);
+			entry.setFormat(null, 24, FlxColor.WHITE, CENTER);
+			optionTexts.add(entry);
+		}
 
-        #if mobile
+		info = new FlxText(80, 180 + (options.length * 40) + 40, FlxG.width - 160, 'UP/DOWN - Navigate\nACCEPT - Open\nBACK - Return to Menu', 18);
+		info.setFormat(null, 18, FlxColor.GRAY, CENTER);
+		add(info);
+
+		#if mobile
 		addCustomDPad();
 		addCustomDPadCam();
 		#end
-    }
 
-    override public function update(elapsed:Float):Void
-    {
-        super.update(elapsed);
+		refreshSelection();
+	}
 
-        if (controls.BACK)
-            FlxG.switchState(new MenuState());
-    }
+	private function buildOptions():Void
+	{
+		options = [
+			{ label: 'Level Editor', action: openLevelEditor },
+			{ label: 'Character Editor', action: openCharacterEditor },
+			{ label: 'Animation Editor', action: openAnimationEditor },
+			{ label: 'Script Editor', action: openScriptEditor }
+		];
+	}
+
+	override public function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+
+		if (controls.GAME_UP_P)
+			moveSelection(-1);
+		else if (controls.GAME_DOWN_P)
+			moveSelection(1);
+
+		if (controls.ACCEPT)
+			options[selectedIndex].action();
+
+		if (controls.BACK)
+			FlxG.switchState(new MenuState());
+	}
+
+	private function moveSelection(direction:Int):Void
+	{
+		selectedIndex = FlxMath.wrap(selectedIndex + direction, 0, options.length - 1);
+		refreshSelection();
+	}
+
+	private function refreshSelection():Void
+	{
+		var i = 0;
+		for (entry in optionTexts.members)
+		{
+			var selected = (i == selectedIndex);
+			entry.color = selected ? FlxColor.YELLOW : FlxColor.WHITE;
+			entry.scale.set(selected ? 1.1 : 1, selected ? 1.1 : 1);
+			i++;
+		}
+	}
+
+	private function openLevelEditor():Void
+	{
+		FlxG.switchState(new LevelEditorState());
+	}
+
+	private function openCharacterEditor():Void
+	{
+	}
+
+	private function openAnimationEditor():Void
+	{
+	}
+
+	private function openScriptEditor():Void
+	{
+	}
 }
