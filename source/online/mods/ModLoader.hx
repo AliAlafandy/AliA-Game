00c50c799f3a9f9d512539bdb0ea500710e67b2b
Ethantobot <ethantobot7@gmail.com>
2026-08-28 04:53:48 +0200

    Refactor loadAllMods for better clarity
    Refactored loadAllMods function to improve readability and structure.

---
+++ source/online/mods/ModLoader.hx
@@ -0,0 +1,44 @@
+package online.mods;
+
+#if sys
+import sys.FileSystem;
+import sys.io.File;
+import haxe.Json;
+import online.mods.ModInstaller.ModMeta;
+
+class ModLoader {
+	public static var loadedMods:Array<ModMeta> = [];
+
+	public static function loadAllMods():Void
+  {
+      loadedMods = [];
+      var modsDir = getModsDirectory();
+  
+      if (!FileSystem.exists(modsDir)) return;
+  
+      for (item in FileSystem.readDirectory(modsDir))
+      {
+          var fullPath = modsDir + "/" + item;
+          
+          if (FileSystem.isDirectory(fullPath) && !item.startsWith("."))
+          {
+              var meta = readMeta(item);
+              if (meta != null)
+              {
+                  loadedMods.push(meta);
+              }
+          }
+  		}
+  }
+
+	public static function getLoadedMods():Array<ModMeta>
+	{
+		return loadedMods;
+	}
+
+	static function getModsDirectory():String
+	{
+		return #if mobile StorageUtil.getStorageDirectory() + #end "mods";
+	}
+}
+#end
