970b54a1aab2227408c3ee0a325f0ff7277364d8
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-08-29 21:12:01 +0300

    Refactor SelectState to use dynamic save display

--- source/states/menus/SelectState.hx
+++ source/states/menus/SelectState.hx
@@ -2,7 +2,18 @@ package states.menus;
 
 import states.MenuState;
 
-class SelectState extends GameState {
+class SelectState extends GameState
+{
+	private final saves:Array<String> = [
+        'NEW GAME 1',
+        'NEW GAME 2',
+        'NEW GAME 3',
+        'NEW GAME 4',
+        'NEW GAME 5',
+        'NEW GAME 6',
+        'NEW GAME 7',
+        'NEW GAME 8'
+    ];
 
     #if mobile
     public var manager:MobileInputManager;
@@ -20,22 +31,9 @@ class SelectState extends GameState {
 		var bg:FlxSprite = new FlxSprite(0, 0).loadGraphic(Paths.image('menus/background'));
 		add(bg);
 
-        var t = new FlxText(0, 0, 0,
-        	"SELECT SAVE\n
-        	\n
-        	NEW GAME 1\n
-        	NEW GAME 2\n
-        	NEW GAME 3\n
-        	NEW GAME 4\n
-        	NEW GAME 5\n
-        	NEW GAME 6\n
-        	NEW GAME 7\n
-        	NEW GAME 8\n
-        	\n
-        	ENTER to Continue",
-         14);
-        t.screenCenter();
-        add(t);
+        var text = new FlxText(0, 0, 0, "", 14);
+        text.screenCenter();
+        add(text);
 
         #if mobile
 		addCustomDPad('EXITE', 'MENU');
@@ -45,12 +43,50 @@ class SelectState extends GameState {
 
     override public function update(elapsed:Float) {
         super.update(elapsed);
+
+		if (controls.UI_UP_P)
+		{
+			selected--;
+
+			if (selected < 0)
+				selected = options.length - 1;
+
+			refresh();
+		}
+
+		if (controls.UI_DOWN_P)
+		{
+			selected++;
+
+			if (selected >= options.length)
+				selected = 0;
+
+			refresh();
+		}
+
         if (controls.ACCEPT) {
             GameState.switchState(new CharacterState());
 			FlxG.sound.music.volume = 0;
         }
+
         if (controls.BACK) {
             GameState.switchState(new MenuState());
         }
     }
+
+	private function refresh():Void
+    {
+        var output:String = 'SELECT SAVE\n\n';
+
+        for (i in 0...saves.length)
+        {
+            var prefix:String = i == selected ? '> ' : '  ';
+            output += prefix + saves[i] + '\n';
+        }
+
+        output += '\nUP / DOWN - Select\nENTER - Open\nESC - Back';
+
+        text.text = output;
+        text.screenCenter();
+	}
 }
