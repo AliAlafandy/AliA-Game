351dbe109399b0812448714dd9f9265fe7052626
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-08-30 06:25:13 -0300

    Refactor Main class and add argument parsing
    Refactor Main class to use BootConfig type for game configuration. Added argument parsing for width, height, fullscreen, and no-splash options.

--- source/Main.hx
+++ source/Main.hx
@@ -5,10 +5,12 @@ import flixel.graphics.FlxGraphic;
 import flixel.FlxGame;
 import flixel.FlxState;
 import haxe.io.Path;
+import haxe.Timer;
 import openfl.Assets;
 import openfl.Lib;
 import openfl.display.Sprite;
 import openfl.events.Event;
+import openfl.events.FocusEvent;
 import openfl.display.StageScaleMode;
 import lime.system.System as LimeSystem;
 import lime.app.Application;
@@ -17,6 +19,13 @@ import openfl.events.KeyboardEvent;
 #if mobile
 import mobile.StorageUtil;
 #end
+
+#if sys
+import sys.io.File;
+import sys.FileSystem;
+#end
+
+import online.MultiPlayer;
 
 import states.TitleState;
 
@@ -29,18 +38,36 @@ import lime.graphics.Image;
 ')
 #end
 
-class Main extends Sprite {
-    var game = {
-		width: 1280, // WINDOW width
-		height: 720, // WINDOW height
-		initialState: TitleState, // initial game state
-		zoom: -1.0, // game state bounds
-		framerate: 60, // default framerate
-		skipSplash: true, // if the default flixel splash screen should be skipped
-		startFullscreen: false // if the game should start at fullscreen mode
+typedef BootConfig =
+{
+	width:Int,
+	height:Int,
+	initialState:Class<FlxState>,
+	zoom:Float,
+	framerate:Int,
+	skipSplash:Bool,
+	startFullscreen:Bool
+}
+
+class Main extends Sprite
+{
+	private static inline var DEFAULT_WIDTH:Int = 1280;
+	private static inline var DEFAULT_HEIGHT:Int = 720;
+	private static inline var RESIZE_DEBOUNCE:Float = 0.15;
+
+	var game:BootConfig = {
+		width: DEFAULT_WIDTH,
+		height: DEFAULT_HEIGHT,
+		initialState: TitleState,
+		zoom: -1.0,
+		framerate: 60,
+		skipSplash: true,
+		startFullscreen: false
 	};
 
 	public static var fpsVar:FPSCounter;
+	public static var safeMode:Bool = false;
+	public static var bootSucceeded:Bool = false;
 
 	#if mobile
 	public static final platform:String = "Phones";
@@ -48,48 +75,147 @@ class Main extends Sprite {
 	public static final platform:String = "PCs";
 	#end
 
-    public static function main():Void
+	private var resizeTimer:Timer;
+
+	public static function main():Void
 	{
 		Lib.current.addChild(new Main());
+
 		#if cpp
-        cpp.NativeGc.enable(true);
-        cpp.NativeGc.run(true);
-        #end
+		cpp.NativeGc.enable(true);
+		cpp.NativeGc.run(true);
+		#end
 	}
 
-    public function new() {
-        super();
+	public function new()
+	{
+		super();
+
+		parseArguments();
 
 		#if mobile
 		MobileData.init();
 		#if android
 		StorageUtil.requestPermissions();
 		#end
-		//Sys.setCwd(StorageUtil.getStorageDirectory());
 		#end
+
 		CrashHandler.init();
 
-        if (stage != null)
-		{
+		if (stage != null)
 			init();
+		else
+			addEventListener(Event.ADDED_TO_STAGE, init);
+	}
+
+	private function parseArguments():Void
+	{
+		#if sys
+		var args = Sys.args();
+
+		for (arg in args)
+		{
+			if (arg == "--safe-mode")
+				safeMode = true;
+
+			if (arg.startsWith("--width="))
+				game.width = Std.parseInt(arg.substr(8));
+
+			if (arg.startsWith("--height="))
+				game.height = Std.parseInt(arg.substr(9));
+
+			if (arg == "--fullscreen")
+				game.startFullscreen = true;
+
+			if (arg == "--no-splash")
+				game.skipSplash = true;
+		}
+
+		if (game.width <= 0) game.width = DEFAULT_WIDTH;
+		if (game.height <= 0) game.height = DEFAULT_HEIGHT;
+		#end
+	}
+
+	private function init(?E:Event):Void
+	{
+		if (hasEventListener(Event.ADDED_TO_STAGE))
+			removeEventListener(Event.ADDED_TO_STAGE, init);
+
+		try
+		{
+			setupGame();
+			bootSucceeded = true;
+		}
+		catch (e:Dynamic)
+		{
+			handleBootFailure(e);
+		}
+	}
+
+	private function handleBootFailure(error:Dynamic):Void
+	{
+		#if sys
+		try
+		{
+			var log = 'Boot failure: ${Std.string(error)}\n${haxe.CallStack.toString(haxe.CallStack.exceptionStack())}';
+			File.saveContent('crash_boot.log', log);
+		}
+		catch (e:Dynamic) {}
+		#end
+
+		if (!safeMode)
+		{
+			safeMode = true;
+			try
+			{
+				setupGame();
+				bootSucceeded = true;
+			}
+			catch (fallbackError:Dynamic)
+			{
+				LimeSystem.exit(1);
+			}
 		}
 		else
 		{
-			addEventListener(Event.ADDED_TO_STAGE, init);
+			LimeSystem.exit(1);
 		}
-    }
-
-    private function init(?E:Event):Void
-	{
-		if (hasEventListener(Event.ADDED_TO_STAGE))
-		{
-			removeEventListener(Event.ADDED_TO_STAGE, init);
-		}
-
-		setupGame();
 	}
 
-    private function setupGame():Void
+	private function setupGame():Void
+	{
+		resolveWindowScale();
+
+		Controls.instance = new Controls();
+		ClientPrefs.loadDefaultKeys();
+
+		#if ACHIEVEMENTS_ALLOWED
+		Achievements.load();
+		#end
+
+		if (MultiPlayer.instance == null)
+			new MultiPlayer();
+
+		var initialState = safeMode ? TitleState : game.initialState;
+
+		addChild(new FlxGame(
+			game.width,
+			game.height,
+			initialState,
+			#if (flixel < "5.0.0") game.zoom, #end
+			game.framerate,
+			game.framerate,
+			game.skipSplash,
+			game.startFullscreen
+		));
+
+		setupDebugOverlay();
+		setupStageProperties();
+		setupPlatformFlags();
+		setupEventListeners();
+	}
+
+	private function resolveWindowScale():Void
 	{
 		#if (openfl <= "9.2.0")
 		var stageWidth:Int = Lib.current.stage.stageWidth;
@@ -107,36 +233,40 @@ class Main extends Sprite {
 		if (game.zoom == -1.0)
 			game.zoom = 1.0;
 		#end
-	
-		Controls.instance = new Controls();
-		ClientPrefs.loadDefaultKeys();
-		#if ACHIEVEMENTS_ALLOWED Achievements.load(); #end
-		addChild(new FlxGame(game.width, game.height, game.initialState, #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
+	}
 
+	private function setupDebugOverlay():Void
+	{
 		fpsVar = new FPSCounter(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
+		fpsVar.visible = ClientPrefs.data.showFPS;
+	}
+
+	private function setupStageProperties():Void
+	{
 		Lib.current.stage.align = "tl";
 		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
-		if(fpsVar != null) {
-			fpsVar.visible = ClientPrefs.data.showFPS;
-		}
 
 		#if linux
-		var icon = Image.fromFile("icon.png");
-		Lib.current.stage.window.setIcon(icon);
+		try
+		{
+			var icon = Image.fromFile("icon.png");
+			Lib.current.stage.window.setIcon(icon);
+		}
+		catch (e:Dynamic) {}
 		#end
+	}
 
-		#if desktop
-		FlxG.stage.addEventListener(KeyboardEvent.KEY_UP, toggleFullScreen);
-		#end
-
+	private function setupPlatformFlags():Void
+	{
 		#if html5
 		FlxG.autoPause = false;
 		FlxG.mouse.visible = false;
 		#end
 
 		#if DISCORD_ALLOWED
-		DiscordClient.prepare();
+		if (!safeMode)
+			DiscordClient.prepare();
 		#end
 
 		#if android
@@ -146,32 +276,91 @@ class Main extends Sprite {
 		#if mobile
 		LimeSystem.allowScreenTimeout = ClientPrefs.data.screensaver;
 		#end
-
-		// shader coords fix
-		FlxG.signals.gameResized.add(function (w, h) {
-			if(fpsVar != null)
-				fpsVar.positionFPS(10, 3, Math.min(Lib.current.stage.stageWidth / FlxG.width, Lib.current.stage.stageHeight / FlxG.height));
-		     if (FlxG.cameras != null) {
-			   for (cam in FlxG.cameras.list) {
-				if (cam != null && cam.filters != null)
-					resetSpriteCache(cam.flashSprite);
-			   }
-			}
-
-			if (FlxG.game != null)
-			resetSpriteCache(FlxG.game);
-		});
 	}
 
-	static function resetSpriteCache(sprite:Sprite):Void {
-		@:privateAccess {
-		        sprite.__cacheBitmap = null;
+	private function setupEventListeners():Void
+	{
+		#if desktop
+		FlxG.stage.addEventListener(KeyboardEvent.KEY_UP, toggleFullScreen);
+		FlxG.stage.addEventListener(FocusEvent.FOCUS_OUT, onWindowBlur);
+		FlxG.stage.addEventListener(FocusEvent.FOCUS_IN, onWindowFocus);
+		#end
+
+		Lib.current.stage.addEventListener(Event.RESIZE, onStageResize);
+
+		FlxG.signals.gameResized.add(onGameResized);
+
+		Lib.current.addEventListener(Event.DEACTIVATE, onApplicationDeactivate);
+		Lib.current.addEventListener(Event.ACTIVATE, onApplicationActivate);
+	}
+
+	private function onStageResize(event:Event):Void
+	{
+		if (resizeTimer != null)
+			resizeTimer.stop();
+
+		resizeTimer = Timer.delay(commitResize, Math.round(RESIZE_DEBOUNCE * 1000));
+	}
+
+	private function commitResize():Void
+	{
+		if (fpsVar != null)
+			fpsVar.positionFPS(10, 3, Math.min(Lib.current.stage.stageWidth / FlxG.width, Lib.current.stage.stageHeight / FlxG.height));
+	}
+
+	private function onGameResized(w:Int, h:Int):Void
+	{
+		if (fpsVar != null)
+			fpsVar.positionFPS(10, 3, Math.min(Lib.current.stage.stageWidth / FlxG.width, Lib.current.stage.stageHeight / FlxG.height));
+
+		if (FlxG.cameras != null)
+		{
+			for (cam in FlxG.cameras.list)
+			{
+				if (cam != null && cam.filters != null)
+					resetSpriteCache(cam.flashSprite);
+			}
+		}
+
+		if (FlxG.game != null)
+			resetSpriteCache(FlxG.game);
+	}
+
+	private function onWindowBlur(event:FocusEvent):Void
+	{
+		#if MULTIPLAYER_ALLOWED
+		if (MultiPlayer.instance != null && MultiPlayer.instance.isConnected())
+			MultiPlayer.instance.sendPlayerUpdate(0, 0, "away");
+		#end
+	}
+
+	private function onWindowFocus(event:FocusEvent):Void
+	{
+	}
+
+	private function onApplicationDeactivate(event:Event):Void
+	{
+		#if cpp
+		cpp.NativeGc.run(false);
+		#end
+	}
+
+	private function onApplicationActivate(event:Event):Void
+	{
+	}
+
+	static function resetSpriteCache(sprite:Sprite):Void
+	{
+		@:privateAccess
+		{
+			sprite.__cacheBitmap = null;
 			sprite.__cacheBitmapData = null;
 		}
 	}
 
-	function toggleFullScreen(event:KeyboardEvent){
-		if(Controls.instance.justReleased('fullscreen'))
+	function toggleFullScreen(event:KeyboardEvent):Void
+	{
+		if (Controls.instance.justReleased('fullscreen'))
 			FlxG.fullscreen = !FlxG.fullscreen;
 	}
 }
