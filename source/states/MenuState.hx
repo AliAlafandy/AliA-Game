b160ef7278b813d55afc91b9c467af0a3ae4caeb
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2026-09-13 14:58:03 +0300

    Resume music if already playing before playing menu music

--- source/states/MenuState.hx
+++ source/states/MenuState.hx
@@ -118,7 +118,9 @@ class MenuState extends GameState
 	override function update(elapsed:Float)
 	{
 		if (FlxG.sound.music == null)
-			FlxG.sound.playMusic(Paths.music('menus/menu'));
+			FlxG.sound.music.resume();
+
+		FlxG.sound.playMusic(Paths.music('menus/menu'));
 
 		if (!selectedSomethin)
 		{
